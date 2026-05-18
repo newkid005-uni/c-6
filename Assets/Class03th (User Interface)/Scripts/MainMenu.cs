ad47e7e804115a5db51977aad4b99d0d50b2aa3c
newkid005-uni <newkid005@gmail.com>
2026-05-18 21:43:57 +0900

    2026.05.18 수출용 패키지

---
+++ Assets/Class03th (User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Start");
+    }
+
+    public void OpenGuide()
+    {
+        Debug.Log("Guide");
+    }
+
+    public void OpenOption()
+    {
+        Debug.Log("Option");
+    }
+
+    public void OpenCredit()
+    {
+        Debug.Log("Credit");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+    }
+
+}
