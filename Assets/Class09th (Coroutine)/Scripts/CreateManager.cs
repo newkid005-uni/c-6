7393552c7a98ee7dc4f7fe6253ca8715de962268
newkid005-uni <newkid005@gmail.com>
2026-06-03 21:44:19 +0900

    2026.06.03 코루틴

--- Assets/Class09th (Coroutine)/Scripts/CreateManager.cs
+++ Assets/Class09th (Coroutine)/Scripts/CreateManager.cs
@@ -1,21 +1,35 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateManager : MonoBehaviour
 {
-
+    
     [SerializeField] GameObject elemental;
-
+    [SerializeField] List<GameObject> monsters;
+    [SerializeField] int index = 0;
+    [SerializeField] float time = 0;
     void Start()
     {
+        
         for(int i = 0; i<5;i++)
         {
-        GameObject monster = Instantiate(elemental,transform);
-        monster.transform.position = new Vector3(i * 1.5f - 3, 0, 0);
+            GameObject clone = Instantiate(elemental,transform);
+            clone.transform.position = new Vector3(i * 1.5f - 3, 0, 0);
+            clone.SetActive(false);
+            monsters.Add(clone);
+
         }
+        StartCoroutine(Coroutine());
     }
 
-    void Update()
+    IEnumerator Coroutine()
     {
-        
+        Debug.Log("Start Coroutine");
+
+        yield return new WaitForSeconds(5.0f);
+
+        Debug.Log("Stop Coroutine");
     }
 }
